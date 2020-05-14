a4d3350877cdb7c9adae022ebba74278ff075cea
raulrocha2 <raul.rocha2@gmail.com>
2020-05-14 17:41:28 -0300

    first commit full folder

---
+++ api_rest/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from core.views import PessoaViewSet
+from rest_framework import routers, serializers, viewsets
+
+router = routers.DefaultRouter()
+router.register(r'pessoas', PessoaViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api-auth', include('rest_framework.urls'))
+]
